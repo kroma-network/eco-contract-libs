b811a328337bb7b25c621c3b8b4141b111f2aeac
seinmyung25 <seinmyung25@gmail.com>
2023-12-18 18:50:12 +0900

    feat: compile versions

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,19 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.22",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.22",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000000,
+          },
+        },
+      },
+    ],
+  },
 };
 
 export default config;
