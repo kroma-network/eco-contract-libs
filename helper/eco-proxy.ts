450440203285d4048bb0f7f65a8234ab420968db
seinmyung25 <seinmyung25@gmail.com>
2024-01-10 14:58:46 +0900

    feat: proxy test coverage all

--- helper/eco-proxy.ts
+++ helper/eco-proxy.ts
@@ -1,12 +1,17 @@
-import { ethers } from "hardhat";
-import { BaseContract, Signer, BytesLike } from "ethers";
-import { EcoTUPWithAdmin, EcoTUPWithAdminLogic__factory } from "../typechain-types";
-import { EcoProxyAdmin__factory, EcoProxyAdmin } from "../typechain-types";
-
-import { AsyncConstructor } from 'async-constructor';
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+import { AsyncConstructor } from "async-constructor";
+import { BaseContract, Signer, BytesLike } from "ethers";
+import hre from "hardhat";
 
-export  interface EcoProxyProperties<CT> { // Generic for Contract logic type CT
+import {
+  EcoTUPWithAdmin,
+  EcoTUPWithAdminLogic__factory,
+  EcoProxyAdmin__factory,
+  EcoProxyAdmin,
+} from "../typechain-types";
+
+export interface EcoProxyProperties<CT> {
+  // Generic for Contract logic type CT
   ecoProxy: EcoTUPWithAdmin;
   ecoProxyAdmin: EcoProxyAdmin;
   ecoLogic: CT;
@@ -19,25 +24,27 @@ export class ProxyFactory extends AsyncConstructor {
   IMPLEMENTATION_SLOT = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
   ADMIN_SLOT = "0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103";
 
-  deployer!: Signer
-  proxyAdminLogic!: EcoProxyAdmin
-  proxyAdminFactory!: EcoProxyAdmin__factory
+  deployer!: Signer;
+  proxyAdminLogic!: EcoProxyAdmin;
+  proxyAdminFactory!: EcoProxyAdmin__factory;
 
-  proxyFactory!: EcoTUPWithAdminLogic__factory
+  proxyFactory!: EcoTUPWithAdminLogic__factory;
 
   constructor(deployer?: HardhatEthersSigner, proxyAdminLogic?: EcoProxyAdmin) {
     super(async () => {
-      this.deployer = deployer ? deployer : (await ethers.getSigners())[0];
-      this.proxyAdminFactory = (await ethers.getContractFactory("EcoProxyAdmin")).connect(this.deployer);
+      this.deployer = deployer ? deployer : (await hre.ethers.getSigners())[0];
+      this.proxyAdminFactory = (await hre.ethers.getContractFactory("EcoProxyAdmin")).connect(this.deployer);
 
-      this.proxyAdminLogic = proxyAdminLogic ? proxyAdminLogic?.connect(deployer) : (await this.proxyAdminFactory.deploy(this.deployer)).connect(this.deployer);
-      this.proxyFactory = (await ethers.getContractFactory("EcoTUPWithAdminLogic")).connect(this.deployer);
+      this.proxyAdminLogic = proxyAdminLogic
+        ? proxyAdminLogic?.connect(deployer)
+        : (await this.proxyAdminFactory.deploy(this.deployer)).connect(this.deployer);
+      this.proxyFactory = (await hre.ethers.getContractFactory("EcoTUPWithAdminLogic")).connect(this.deployer);
     });
   }
 
   async deploy<CT extends BaseContract>(logic: CT, input: BytesLike): Promise<EcoProxiedInstance<CT>> {
     const proxy = await this.proxyFactory.deploy(this.proxyAdminLogic, logic, input);
-    const inst = await logic.attach(proxy) as EcoProxiedInstance<CT>;
+    const inst = (await logic.attach(proxy)) as EcoProxiedInstance<CT>;
 
     inst.ecoProxy = proxy;
     inst.ecoProxyAdmin = this.proxyAdminFactory.attach(await this.getAdminAddress(proxy)) as EcoProxyAdmin;
@@ -46,24 +53,24 @@ export class ProxyFactory extends AsyncConstructor {
     return inst;
   }
 
-  async getImplAddress<CT extends BaseContract>(logic:CT) {
-    const slotData = await ethers.provider.getStorage(logic, this.IMPLEMENTATION_SLOT)
-    return ethers.getAddress("0x" + slotData.slice(-40));
+  async getImplAddress<CT extends BaseContract>(logic: CT) {
+    const slotData = await hre.ethers.provider.getStorage(logic, this.IMPLEMENTATION_SLOT);
+    return hre.ethers.getAddress("0x" + slotData.slice(-40));
   }
 
-  async getAdminAddress<CT extends BaseContract>(logic:CT) {
-    const slotData = await ethers.provider.getStorage(logic, this.ADMIN_SLOT)
-    return ethers.getAddress("0x" + slotData.slice(-40));
+  async getAdminAddress<CT extends BaseContract>(logic: CT) {
+    const slotData = await hre.ethers.provider.getStorage(logic, this.ADMIN_SLOT);
+    return hre.ethers.getAddress("0x" + slotData.slice(-40));
   }
 }
 
 async function test() {
-  const logicFactory = await ethers.getContractFactory("EcoERC20Mintable");
+  const logicFactory = await hre.ethers.getContractFactory("EcoERC20Mintable");
   const proxyFactory = new ProxyFactory();
-  const logic = await logicFactory.deploy('a', 'a');
-  const input = logic.interface.encodeFunctionData("initEcoERC20Mintable", [logic, 'a', 'a']);
+  const logic = await logicFactory.deploy("a", "a");
+  const input = logic.interface.encodeFunctionData("initEcoERC20Mintable", [logic, "a", "a"]);
   const inst = await proxyFactory.deploy(logic, input);
 
-  inst.symbol();
-  inst.ecoLogic.symbol();
+  await inst.symbol();
+  await inst.ecoLogic.symbol();
 }
