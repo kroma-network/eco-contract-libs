180b3a341d7446088527c140cee765f75aa30a02
seinmyung25 <seinmyung25@gmail.com>
2024-01-23 15:18:17 +0900

    add: hardhat node support brownie

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -20,6 +20,16 @@ const config: HardhatUserConfig = {
       },
     ],
   },
+  networks: {
+    hardhat: {
+      hardfork: "london",
+      // base fee of 0 allows use of 0 gas price when testing
+      initialBaseFeePerGas: 0,
+      // brownie expects calls and transactions to throw on revert
+      throwOnTransactionFailures: true,
+      throwOnCallFailures: true
+    }
+  }
 };
 
 export default config;
