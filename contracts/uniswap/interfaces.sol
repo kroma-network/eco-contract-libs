36dec49c9cf300921c0a6ea761c5161f59592ce0
seinmyung25 <seinmyung25@gmail.com>
2024-03-17 21:04:47 +0900

    feat: uniswap base interfaces

---
+++ contracts/uniswap/interfaces.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v5.0.0) (proxy/transparent/ProxyAdmin.sol)
+
+pragma solidity ^0.8.20;
+
+import "@uniswap/universal-router/contracts/interfaces/IUniversalRouter.sol";
