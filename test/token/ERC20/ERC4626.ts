7f8c651c1f45f20af0218d279822a7ae8250e9e0
seinmyung25 <seinmyung25@gmail.com>
2024-05-03 16:50:47 +0900

    coverage: erc4626

--- test/token/ERC20/ERC4626.ts
+++ test/token/ERC20/ERC4626.ts
@@ -1,4 +1,3 @@
-
 import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
 import { MaxUint256, ZeroAddress } from "ethers";
@@ -36,6 +35,8 @@ describe("ERC4626 The ERC20 that Wrapping ERC20 for Support Stake and Shares", f
       expect(await erc4626Token.previewMint(0n)).to.equal(0n);
       expect(await erc4626Token.previewRedeem(0n)).to.equal(0n);
       expect(await erc4626Token.previewWithdraw(0n)).to.equal(0n);
+
+      expect(await erc4626Token.nonces(owner)).to.equal(0n);
     });
 
     it("basic fail", async function () {
@@ -44,8 +45,36 @@ describe("ERC4626 The ERC20 that Wrapping ERC20 for Support Stake and Shares", f
       await expect(erc4626Token.connect(users[0]).transfer(users[1], amount)).reverted;
     });
   });
+  describe("ERC20 Feature of ERC4626", function () {
+    it("TransferFrom", async function () {
+      const { owner, erc20, erc4626Token, users } = await loadFixture(ERC4626_with_Token_Fixture);
 
-  describe("ERC20 Rebase Feature", function () {
+      await expect(erc4626Token.connect(users[1]).deposit(await erc20.balanceOf(users[1]), users[1])).not.reverted;
+      const actionAmount = await erc4626Token.balanceOf(users[1]);
+      expect(actionAmount).not.eq(0n);
+
+      await expect(erc4626Token.connect(users[0]).transferFrom(users[1], users[0], actionAmount)).reverted;
+      await expect(erc4626Token.connect(users[1]).approve(users[0], actionAmount)).not.reverted;
+      await expect(erc4626Token.connect(users[0]).transferFrom(users[1], users[0], actionAmount)).not.reverted;
+    });
+
+    it("Burn", async function () {
+      const { owner, erc20, erc4626Token, users } = await loadFixture(ERC4626_with_Token_Fixture);
+
+      await expect(erc4626Token.connect(users[0]).deposit(await erc20.balanceOf(users[0]), users[0])).not.reverted;
+      const actionAmount = (await erc4626Token.balanceOf(users[0])) / 2n;
+      expect(actionAmount).not.eq(0n);
+
+      await expect(erc4626Token.connect(users[0]).burn(actionAmount)).not.reverted;
+
+      await expect(erc4626Token.connect(users[0]).burnFrom(users[0], actionAmount)).reverted;
+      await expect(erc4626Token.burnFrom(users[0], actionAmount)).reverted;
+      await expect(erc4626Token.connect(users[0]).approve(owner, actionAmount)).not.reverted;
+      await expect(erc4626Token.burnFrom(users[0], actionAmount)).not.reverted;
+    });
+  });
+
+  describe("ERC4626 Feature", function () {
     it("Stake & Reward & Stake", async function () {
       const { owner, erc20, erc4626Token, users } = await loadFixture(ERC4626_with_Token_Fixture);
 
@@ -55,24 +84,24 @@ describe("ERC4626 The ERC20 that Wrapping ERC20 for Support Stake and Shares", f
       expect(await erc4626Token.balanceOf(users[0])).eq(amount);
 
       await expect(erc4626Token.connect(users[1]).deposit(amount, users[1])).not.reverted;
-      expect(await erc4626Token.totalSupply()).eq(amount*2n);
+      expect(await erc4626Token.totalSupply()).eq(amount * 2n);
       expect(await erc4626Token.balanceOf(users[0])).eq(amount);
       expect(await erc4626Token.balanceOf(users[1])).eq(amount);
 
       // add reward
-      await expect(erc20.transfer(erc4626Token, amount*2n)).not.reverted;
-      expect(await erc4626Token.totalSupply()).eq(amount*2n);
+      await expect(erc20.transfer(erc4626Token, amount * 2n)).not.reverted;
+      expect(await erc4626Token.totalSupply()).eq(amount * 2n);
       expect(await erc4626Token.balanceOf(users[0])).eq(amount);
       expect(await erc4626Token.balanceOf(users[1])).eq(amount);
 
-      await expect(erc4626Token.connect(users[3]).deposit(amount*2n, users[3])).not.reverted;
-      expect(await erc4626Token.totalSupply()).eq(amount*3n);
+      await expect(erc4626Token.connect(users[3]).deposit(amount * 2n, users[3])).not.reverted;
+      expect(await erc4626Token.totalSupply()).eq(amount * 3n);
       expect(await erc4626Token.balanceOf(users[0])).eq(amount);
       expect(await erc4626Token.balanceOf(users[1])).eq(amount);
       expect(await erc4626Token.balanceOf(users[3])).eq(amount);
 
       await expect(erc4626Token.connect(users[3]).redeem(amount, users[3], users[3])).not.reverted;
-      expect(await erc4626Token.totalSupply()).eq(amount*2n);
+      expect(await erc4626Token.totalSupply()).eq(amount * 2n);
       expect(await erc4626Token.balanceOf(users[0])).eq(amount);
       expect(await erc4626Token.balanceOf(users[1])).eq(amount);
       expect(await erc4626Token.balanceOf(users[3])).eq(0n);
@@ -116,7 +145,7 @@ describe("ERC4626 The ERC20 that Wrapping ERC20 for Support Stake and Shares", f
       await expect(erc20.transfer(erc4626Token, amount)).not.reverted;
       expect(await erc4626Token.totalSupply()).eq(amount);
       expect(await erc4626Token.balanceOf(users[0])).eq(amount);
-      expect(await erc20.balanceOf(erc4626Token)).eq(amount*2n);
+      expect(await erc20.balanceOf(erc4626Token)).eq(amount * 2n);
 
       await expect(erc4626Token.connect(users[0]).redeem(amount, users[0], users[0])).not.reverted;
       expect(await erc4626Token.totalSupply()).eq(0n);
@@ -124,9 +153,9 @@ describe("ERC4626 The ERC20 that Wrapping ERC20 for Support Stake and Shares", f
       expect(await erc20.balanceOf(erc4626Token)).eq(1n);
 
       await expect(erc4626Token.connect(users[0]).deposit(amount, users[0])).not.reverted;
-      expect(await erc4626Token.totalSupply()).eq(amount/2n);
-      expect(await erc4626Token.balanceOf(users[0])).eq(amount/2n);
-      expect(await erc20.balanceOf(erc4626Token)).eq(amount+1n);
+      expect(await erc4626Token.totalSupply()).eq(amount / 2n);
+      expect(await erc4626Token.balanceOf(users[0])).eq(amount / 2n);
+      expect(await erc20.balanceOf(erc4626Token)).eq(amount + 1n);
     });
   });
 });
