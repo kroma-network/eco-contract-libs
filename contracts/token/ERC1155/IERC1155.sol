6620f08e9a670e84c81783a89bf4666e17b06edc
seinmyung25 <seinmyung25@gmail.com>
2024-06-03 18:17:44 +0900

    feat: ERC721/1155 reciever base

--- contracts/token/ERC1155/IERC1155.sol
+++ contracts/token/ERC1155/IERC1155.sol
@@ -7,6 +7,8 @@ import { ISelectorRoleControl } from "../../access/SelectorRoleControlUpgradeabl
 import { IERC1155 } from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import { IERC1155MetadataURI } from "@openzeppelin/contracts/token/ERC1155/extensions/IERC1155MetadataURI.sol";
 
+import { IERC1155Receiver } from "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+
 interface IERC1155URIControl is ISelectorRoleControl, IERC1155MetadataURI {
     function setURI(uint256 tokenId, string memory tokenURI) external;
 
