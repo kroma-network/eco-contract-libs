a93e284d2c11b92dcbdee1d0e57dbf3c863c6564
seinmyung25 <seinmyung25@gmail.com>
2024-06-14 13:23:27 +0900

    fix: refactor inherits for ERC1155

--- contracts/token/ERC1155/IERC1155.sol
+++ contracts/token/ERC1155/IERC1155.sol
@@ -9,10 +9,10 @@ import { IERC1155MetadataURI } from "@openzeppelin/contracts/token/ERC1155/exten
 
 import { IERC1155Receiver } from "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 
-interface IERC1155URIControl is ISelectorRoleControl, IERC1155MetadataURI {
-    function setURI(uint256 tokenId, string memory tokenURI) external;
+interface IERC1155Burnable is IERC1155 {
+    function burn(address account, uint256 id, uint256 value) external;
 
-    function setBaseURI(string memory baseURI) external;
+    function burnBatch(address account, uint256[] memory ids, uint256[] memory values) external;
 }
 
 interface IERC1155Supply is IERC1155 {
@@ -29,4 +29,10 @@ interface IERC1155Mintable is ISelectorRoleControl, IERC1155 {
     function mintBatch(address to, uint256[] calldata ids, uint256[] calldata values, bytes calldata data) external;
 }
 
-interface IEcoERC1155 is IERC1155Supply, IERC1155Mintable, IERC1155URIControl {}
+interface IERC1155URIControl is ISelectorRoleControl, IERC1155MetadataURI {
+    function setURI(uint256 tokenId, string memory tokenURI) external;
+
+    function setBaseURI(string memory baseURI) external;
+}
+
+interface IEcoERC1155 is IERC1155Burnable, IERC1155Supply, IERC1155Mintable, IERC1155URIControl {}
