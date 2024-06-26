a02fae4d5f71e8db66a016cb1719e9e10785d6a9
seinmyung25 <seinmyung25@gmail.com>
2024-06-26 17:17:46 +0900

    feat: Eco ERC721 URIStorage concat .json

--- contracts/token/ERC721/EcoERC721URIStorageUpgradeable.sol
+++ contracts/token/ERC721/EcoERC721URIStorageUpgradeable.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.20;
 
 import { SelectorRoleControlUpgradeable, AccessControlEnumerableUpgradeable } from "../../access/SelectorRoleControlUpgradeable.sol";
 
-import { IERC165, IEcoERC721URIStorage } from "./IERC721.sol";
+import { IERC165, IERC721Metadata, IEcoERC721URIStorage } from "./IERC721.sol";
 import { ERC721URIStorageUpgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 
 abstract contract EcoERC721URIStorageUpgradeable is
@@ -23,6 +23,12 @@ abstract contract EcoERC721URIStorageUpgradeable is
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override(ERC721URIStorageUpgradeable, IERC721Metadata) returns (string memory) {
+        return string.concat(super.tokenURI(tokenId), ".json");
     }
 
     function setTokenURI(uint256 tokenId, string memory _tokenURI) public virtual override onlyAdmin {
