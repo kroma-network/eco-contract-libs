6c0e12ea8c2ae4130b2db7e69361084009c7d867
seinmyung25 <seinmyung25@gmail.com>
2024-07-03 15:22:27 +0900

    coverage: prng

--- contracts/utils/test.sol
+++ contracts/utils/test.sol
@@ -8,20 +8,16 @@ using Keccak256RG for Keccak256RG.KeccakRGState;
 using LCG10000 for LCG10000.LCGState;
 
 contract TestPRNG {
-    function keccakBytes32(bytes32 word) public pure returns (bytes32) {
-        return keccak256(abi.encode(word));
+    function testBytes32Keccak(bytes32 data) public pure returns (bytes32) {
+        return bytes32Keccak(data);
     }
 
-    function keccakNumber(uint256 number) public pure returns (bytes32) {
-        return keccak256(abi.encode(number));
+    function testReduceXOR32(bytes32 word) public pure returns (bytes32) {
+        return reduceXOR32(word);
     }
 
-    function reduceXOR64(bytes32 data) public pure returns (bytes32) {
-        return bytes32ReduceXOR64(data);
-    }
-
-    function reduceXORbits(bytes32 data, uint256 bits) public pure returns (bytes32) {
-        return reduceXOR(data, bits);
+    function testReduceXOR(bytes32 word, uint256 out_bits) public pure returns (bytes32) {
+        return reduceXOR(word, out_bits);
     }
 
     function genKeccak(bytes32 seed, uint256 count) public pure returns (uint256[] memory randoms) {
@@ -33,13 +29,40 @@ contract TestPRNG {
         }
     }
 
-    function genLCG(bytes32 seed, uint256 count) public pure returns (uint256[] memory randoms) {
+    function genLCGYield(bytes32 seed, uint256 count) public pure returns (uint256[] memory randoms) {
+        randoms = new uint256[](count);
+
+        LCG10000.LCGState memory lcg = LCG10000.init(seed, 10000);
+        for (uint256 i; i < count; i++) {
+            randoms[i] = lcg.yield();
+        }
+    }
+
+    function genLCGYield10000(bytes32 seed, uint256 count) public pure returns (uint256[] memory randoms) {
         randoms = new uint256[](count);
 
         LCG10000.LCGState memory lcg = LCG10000.init(seed, 10000);
         for (uint256 i; i < count; i++) {
             randoms[i] = lcg.yield10000();
         }
+    }
+
+    function genLCGByState(uint128 state, uint256 count) public pure returns (uint256[] memory randoms) {
+        randoms = new uint256[](count);
+
+        LCG10000.LCGState memory lcg = LCG10000.init10000ByState(state);
+        for (uint256 i; i < count; i++) {
+            randoms[i] = lcg.yield10000();
+        }
+    }
+
+    // for branch coverage
+    function updateWeed(Keccak256RG.KeccakRGState memory kst) public pure {
+        return Keccak256RG.updateWeed(kst);
+    }
+
+    function yield16(Keccak256RG.KeccakRGState memory kst) public pure returns (uint16 value) {
+        return Keccak256RG.yield16(kst);
     }
 }
 
