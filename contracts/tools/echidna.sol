fc9cd5677d34d86fe609ad5ddb705f347f007f10
seinmyung25 <seinmyung25@gmail.com>
2024-08-02 17:30:08 +0900

    rm: unnecessary code and coverage

--- contracts/tools/echidna.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-contract Echidna {
-    function echidna_check_balance() public view returns (bool) {
-        return (address(this).balance >= 20);
-    }
-}
