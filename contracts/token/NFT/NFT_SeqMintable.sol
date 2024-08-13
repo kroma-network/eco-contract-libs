188f15a83f6c267b54f40891f0bdc4f949176849
seinmyung25 <seinmyung25@gmail.com>
2024-08-13 18:27:19 +0900

    coverage: ERC721 Identical Metadata and others

--- contracts/token/NFT/NFT_SeqMintable.sol
+++ contracts/token/NFT/NFT_SeqMintable.sol
@@ -2,9 +2,14 @@
 
 pragma solidity ^0.8.0;
 
-import { IERC721SequencialMintUpbradeable, IERC721Queryable, IERC721Burnable } from "../ERC721/IERC721.sol";
+import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import { ERC721Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import { EcoERC721Base } from "../ERC721/EcoERC721Base.sol";
+import { IERC721Metadata, IERC721SequencialMintUpbradeable, IERC721Queryable, IERC721Burnable } from "../ERC721/IERC721.sol";
 import { ERC721SequencialMintUpbradeable } from "../ERC721/ERC721SequencialMintUpbradeable.sol";
 import { EcoERC721SequencialQueryable } from "../ERC721/EcoERC721Queryable.sol";
+
+import { IEcoERC721URIStorage, EcoERC721URIStorageUpgradeable, EcoERC721IdenticalURIUpgradeable } from "../ERC721/EcoERC721URIStorageUpgradeable.sol";
 
 interface INFT_SeqMintable is IERC721Queryable, IERC721SequencialMintUpbradeable {}
 
@@ -25,5 +30,57 @@ contract NFT_SeqMintable is INFT_SeqMintable, EcoERC721SequencialQueryable {
 
     function burn(uint256 tokenId) public virtual override(EcoERC721SequencialQueryable, IERC721Burnable) {
         return super.burn(tokenId);
+    }
+}
+
+contract NFT_SeqMintableIdenticalURI is NFT_SeqMintable, EcoERC721IdenticalURIUpgradeable {
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        virtual
+        override(ERC721SequencialMintUpbradeable, EcoERC721URIStorageUpgradeable, IERC165)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function burn(uint256 tokenId) public virtual override {
+        return super.burn(tokenId);
+    }
+
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal virtual override(ERC721Upgradeable, EcoERC721Base) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(
+        address account,
+        uint128 amount
+    ) internal virtual override(ERC721Upgradeable, EcoERC721Base) {
+        return super._increaseBalance(account, amount);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    )
+        public
+        view
+        virtual
+        override(EcoERC721Base, EcoERC721IdenticalURIUpgradeable, IERC721Metadata)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function setTokenURI(
+        uint256 tokenId,
+        string memory _tokenURI
+    ) public virtual override(EcoERC721IdenticalURIUpgradeable, EcoERC721URIStorageUpgradeable, IEcoERC721URIStorage) {
+        super.setTokenURI(tokenId, _tokenURI);
     }
 }
